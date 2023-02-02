ad0c3c0bd26ee65c9cf525ec2ba971e82dca1773
WonderfulSoap <86732447+WonderfulSoap@users.noreply.github.com>
2023-02-02 22:58:02 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tailscale-with-derp
-a tailscale docker image with derp server
+a tailscale docker image with derp server. It will auto get newest tailscale version, and build docker image in 00:00 every day.
+
+https://hub.docker.com/r/wondersoap/tailscale-with-derp
+
+```Dockerfile
+FROM golang:alpine AS build-env
+
+ARG LATEST_VERSION
+
+RUN go install tailscale.com/cmd/derper@${LATEST_VERSION}
+
+FROM tailscale/tailscale
+COPY --from=build-env $HOME/go/bin/derper /usr/local/bin/derper
+```
+
--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 apt update
 apt install jq -y
+
 latest_version=$(curl -s  https://api.github.com/repos/tailscale/tailscale/releases/latest | jq -r .tag_name)
 echo "the latest version of tailscale is $latest_version"
-docker build . --build-arg LATEST_VERSION=$latest_version -t wondersoap/tailscale-with-derp:$latest_version -t wondersoap/tailscale-with-derp:latest
+
+dockerhub_tags=$(curl -s https://registry.hub.docker.com/v2/repositories/wondersoap/tailscale-with-derp/tags)
+if [[ "$dockerhub_tags" == *"$latest_version"* ]]; then
+    echo "version $latest_version already built adn pushed to docker hub. finish"
+    exit
+fi
+
+# build and push docker image
 echo $DOCKER_HUB_TOKEN | docker login -u wondersoap --password-stdin
+docker build . --build-arg LATEST_VERSION=$latest_version -t wondersoap/tailscale-with-derp:$latest_version -t wondersoap/tailscale-with-derp:latest
 docker push wondersoap/tailscale-with-derp:$latest_version
 docker push wondersoap/tailscale-with-derp:latest
